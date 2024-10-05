da46966035e7f478126642a97d97bab094ed428f
Suzie <Suzie.Brown@ghyston.com>
2024-10-05 10:49:33 +0100

    Set up publish

--- Web/Program.cs
+++ Web/Program.cs
@@ -6,5 +6,12 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddFastEndpoints().SwaggerDocument();
 
 var app = builder.Build();
+app.UseStaticFiles();
 app.UseFastEndpoints(c => { c.Endpoints.RoutePrefix = "api"; }).UseSwaggerGen();
+
+if (!app.Environment.IsDevelopment())
+{
+    app.MapFallbackToFile("index.html");
+}
+
 app.Run();
