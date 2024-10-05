c8ccadce5206f4f221c3cc16d2ab8148d6e749f1
Suzie <Suzie.Brown@ghyston.com>
2024-10-05 10:14:54 +0100

    Add fast endpoints

---
+++ Web/Features/Weather/GetWeather.cs
@@ -0,0 +1,37 @@
+﻿using FastEndpoints;
+
+public class GetWeather
+{
+    public record Response(IEnumerable<WeatherForecast> Forecasts);
+    public record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
+    {
+        public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+    }
+    
+    public class Endpoint : EndpointWithoutRequest<Response>
+    {
+        public override void Configure()
+        {
+            Get("/weather");
+            AllowAnonymous();
+        }
+
+        public override Task<Response> ExecuteAsync(CancellationToken cancellationToken)
+        {
+            var summaries = new[]
+            {
+                "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+            };
+            
+            var forecast = Enumerable.Range(1, 5).Select(index =>
+                    new WeatherForecast
+                    (
+                        DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
+                        Random.Shared.Next(-20, 55),
+                        summaries[Random.Shared.Next(summaries.Length)]
+                    ))
+                .ToArray();
+            return Task.FromResult(new Response(forecast));   
+        }
+    }
+}
