bc25bd777e113fedd80cff943147f720a9777170
Suzie <Suzie.Brown@ghyston.com>
2024-10-05 15:16:04 +0100

    Add database models for initial schema

---
+++ Web/Features/StarMaps/VisibleStar.cs
@@ -0,0 +1,18 @@
+﻿using Web.Features.Stars;
+
+namespace Web.Features.StarMaps;
+
+public class VisibleStar
+{
+    public int Id { get; set; }
+    
+    public int StarId { get; set; }
+    public Star? Star { get; set; }
+    
+    public int StarMapId { get; set; }
+    public StarMap? StarMap { get; set; }
+    
+    public decimal X { get; set; }
+    public decimal Y { get; set; }
+    public decimal Brightness { get; set; }
+}
--- Web/Features/Stars/Star.cs
+++ Web/Features/Stars/Star.cs
@@ -2,14 +2,18 @@
 using Microsoft.EntityFrameworkCore;
 using Web.Features.StarMaps;
 
-namespace Web.Features.Planets;
+namespace Web.Features.Stars;
 
-public class Planet
+public class Star
 {
     public int Id { get; set; }
     [Column(TypeName = "VARCHAR(100)")] public string Name { get; set; } = "";
+    
     [Precision(20, 4)] public decimal RightAscensionInDegrees { get; set; }
     [Precision(20, 4)] public decimal DeclinationInDegrees { get; set; }
     [Precision(20, 4)] public decimal DistanceFromEarthInParsecs { get; set; }
-    public ICollection<StarMap>? StarMaps { get; set; }
+    
+    public decimal Luminosity { get; set; }
+    
+    public ICollection<VisibleStar>? VisibleStars { get; set; }
 }
