b31aa5f8d2903cc327cfe27f1ffdc34174870c8d
Yaroslav Polosin <yaroslav.polosin@ghyston.com>
2024-10-05 11:37:26 +0100

    Initial UI

--- Web/client-app/src/App.tsx
+++ Web/client-app/src/App.tsx
@@ -1,13 +1,14 @@
 import * as data from "./data/stars.json";
-import {Star, StarChart} from "./components/StarChart.tsx";
+import { Star, StarChart } from "./components/StarChart.tsx";
+import { Sidebar } from "./components/Sidebar.tsx";
+import { Layout } from "./components/Layout.tsx";
 
-function App(){
-
-  return(
-      <>
-      <div className="bg-red-500">Test</div>
-      <StarChart stars={data.stars as Array<Star>}/>
-    </>
+function App() {
+  return (
+    <Layout>
+      <Sidebar />
+      <StarChart stars={data.stars as Array<Star>} />
+    </Layout>
   );
 }
 
